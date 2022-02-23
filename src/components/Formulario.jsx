13f5a6c63c4c916ba1a097289316ecaa2766e8b7
sam <samueldazadaza@gmail.com>
2022-02-22 21:01:57 -0500

    21- evento unsubmit, campos vacios

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -5,9 +5,22 @@ const Formulario = () => {
     const [nombre, setNombre] = useState('')
     const [edad, setEdad] = useState('')
 
+    const Validar = (event) => {
+        event.preventDefault()
+        console.log("pulsado boton")
+        if(!nombre.trim()){
+            console.log("nombre vacio")
+            return
+        }
+        if(!edad.trim()){
+            console.log("edaddddddddd vacio")
+            return
+        }
+    }
+
   return (
     <div className="container">
-        <form className="form-group">
+        <form onSubmit={Validar} className="form-group">
             <input 
                 placeholder="Introduce el nombre"
                 className="form-control mb-3"
