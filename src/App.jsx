ed193fe53f7cf557b6cc09d7e97299ff31150dd2
sam <samueldazadaza@gmail.com>
2022-02-22 19:05:10 -0500

    archivos iniciales, borrando initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Hola from './components/Hola'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        titleee
+      </h1>
+      <Hola></Hola>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Hola.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Hola() {
+    return (
+        <div>
+            <h2>hellooo</h2>
+            
+        </div>
+    )
+}
+
+export default Hola
