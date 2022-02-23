46d68139c5ea03fd16f7a9afb4f213407bb43a7d
sam <samueldazadaza@gmail.com>
2022-02-22 20:05:27 -0500

    15- list key map

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Contador from './components/Contador'
+import Listado from './components/Listado'
 
 function App() {
   return (
     <div className="App">
       <h1>titleee</h1>
-      <Contador></Contador>
+      <Listado></Listado>
     </div>
   );
 }
---
+++ src/components/Listado.jsx
@@ -0,0 +1,22 @@
+import React, {Fragment, useState} from 'react'
+
+const Listado = () => {
+
+const [numeros, setNumeros] = useState([1,2,3,4,5])
+
+  return (
+    <Fragment>
+        <ul>
+            {
+                numeros.map ( (item, index) =>
+                <li key={index}>
+                    {item} ---- {index}
+                </li>
+                )
+            }
+        </ul>
+    </Fragment>
+  )
+}
+
+export default Listado
